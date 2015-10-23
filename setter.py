c0272c0f58f9b0cbaf9c3f09cca01c05df257b2b
Bob Allred <balinbob@gmail.com>
2015-10-23 17:37:10 -0400

    add license to each file

--- setter.py
+++ setter.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python2
 # vim: set ts=4 sw=4 et ai si:
+#
+#    Bob's Terminator Customization Plugins
+#    Copyright (C) 2015 balinbob@gmail.com
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, version 2 only.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program; if not, write to the Free Software
+#    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  
+#    USA
 
 import pygtk,gtk
 import sys
